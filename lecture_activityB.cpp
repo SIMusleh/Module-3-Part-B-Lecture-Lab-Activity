2697673829dfd3977c7ceb0e911728b0d02c278a
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-14 01:12:05 +0000

    add header and main() with movies

---
+++ lecture_activityB.cpp
@@ -0,0 +1,20 @@
+/* Program name: lecture_activityB.cpp
+ * Author: Sahar Musleh
+ * Date last updated: 11/13/2025
+ * Purpose: Use makeClock() to create a dynamic clock and add it to a movie's showtimes. Clean up memory.
+ */
+
+#include <iostream>
+#include <string>
+#include "movieTimes.h"
+
+int main() {
+    // Create movies and enter initial times
+    MovieTimes predator("Predator: Badlands", 107, "PG-13");
+    predator.enterTimes();
+
+    MovieTimes regrettingYou("Regretting You", 117, "PG-13");
+    regrettingYou.enterTimes();
+
+    MovieTimes sarahsOil("Sarah's Oil", 103, "PG");
+    sarahsOil.enterTimes();
